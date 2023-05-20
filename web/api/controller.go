ab37b70bb254e43d5d1a8954500ccfab7abc3cc2
ilyag26 <gayvoronskiy.i02@gmail.com>
2023-05-20 23:35:40 +0200

    bugs fixed

--- web/api/controller.go
+++ web/api/controller.go
@@ -8,32 +8,59 @@ import (
 	n "github.com/Username/Project-Name/web/scraping"
 )
 
-var tpl = template.Must(template.ParseFiles("templates/index.html"))
-var tpl2 = template.Must(template.ParseFiles("templates/processor.html"))
-var ChannelYou string
+var indexTpl = template.Must(template.ParseFiles("templates/index.gohtml"))
+var videoCheckProcessTpl = template.Must(template.ParseFiles("templates/videoCheckProcess.gohtml"))
+var videoCheckTpl = template.Must(template.ParseFiles("templates/videocheck.gohtml"))
+var channelCheckProcessTpl = template.Must(template.ParseFiles("templates/channelCheckProcess.gohtml"))
+var channelCheckTpl = template.Must(template.ParseFiles("templates/channelcheck.gohtml"))
+var videoURL string
+var channelURL string
 
 func index(w http.ResponseWriter, r *http.Request) {
-	tpl.Execute(w, nil)
+	indexTpl.Execute(w, nil)
 }
-func process(w http.ResponseWriter, r *http.Request) {
+func videoCheck(w http.ResponseWriter, r *http.Request) {
+	videoCheckTpl.Execute(w, nil)
+}
+func channelCheck(w http.ResponseWriter, r *http.Request) {
+	channelCheckTpl.Execute(w, nil)
+}
+func videoCheckProcess(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
-	ChannelYou = r.FormValue("channel")
-	n.Scrape(ChannelYou)
-	data := d.Data{
-		Name:  n.NameRet,
-		Desc:  n.DescRet,
-		Date:  n.DateRet,
-		Views: n.ViewsRet,
+	videoURL = r.FormValue("videoURL")
+	n.ScrapeVideo(videoURL)
+	videoData := d.VideoData{
+		Name:  n.NameVideoRet,
+		Desc:  n.DescVideoRet,
+		Date:  n.DateVideoRet,
+		Views: n.ViewsVideoRet,
 	}
-	tpl2.Execute(w, data)
+	videoCheckProcessTpl.Execute(w, videoData)
+}
+func channelCheckProcess(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	channelURL = r.FormValue("channelURL")
+	n.ScrapeChannel(channelURL)
+	dataChannel := d.ChannelData{
+		Name:  n.NameChannelRet,
+		Desc:  n.DescChannelRet,
+		Views: n.Maind,
+	}
+	channelCheckProcessTpl.Execute(w, dataChannel)
 }
 func StartServer() {
 	fs := http.FileServer(http.Dir("assets/styles"))
 	http.Handle("/styles/", http.StripPrefix("/styles", fs))
 	http.HandleFunc("/", index)
-	http.HandleFunc("/process", process)
+	http.HandleFunc("/channelcheck", channelCheck)
+	http.HandleFunc("/videocheck", videoCheck)
+	http.HandleFunc("/videocheckprocess", videoCheckProcess)
+	http.HandleFunc("/channelcheckprocess", channelCheckProcess)
 	http.ListenAndServe(":8080", nil)
 }
